dc5947769d9dbbc88c1f4546792606483f543f31
31434116 <31434116@qq.com>
2019-12-02 16:13:04 +0800

    use error instead Error

--- cluster/request.go
+++ cluster/request.go
@@ -13,35 +13,35 @@ type request struct {
 	respLimit int64
 }
 
-func (r *request) do() ([]byte, *Error) {
+func (r *request) do() ([]byte, error) {
 	//send header
 	if _, err := r.c.Write(r.header); err != nil {
-		return nil, NewError("WriteRequestHeaderErr", err)
+		return nil, fmt.Errorf("WriteRequestHeaderErr: [%w]", err)
 	}
 	//send body
 	if r.body != nil {
 		if _, err := r.c.Write(r.body); err != nil {
-			return nil, NewError("WriteRequestBodyErr", err)
+			return nil, fmt.Errorf("WriteRequestBodyErr: [%w]", err)
 		}
 	}
 	//receive server response
 	return r.readResponse()
 }
 
-func (r *request) readResponse() ([]byte, *Error) {
+func (r *request) readResponse() ([]byte, error) {
 	//receive response header
 	h := header{}
 	if err := h.read(r.c); err != nil {
-		return nil, NewError("ReadResponseHeaderErr", err)
+		return nil, fmt.Errorf("ReadResponseHeaderErr: [%w]", err)
 	}
 	if r.respLimit > 0 && h.pkgLen > r.respLimit {
 		r.c.MarkUnusable()
-		return nil, NewError("WrongPkgLengthErr", fmt.Errorf("receive header pkg length %d exceed expected or limit size: %d", h.pkgLen, r.respLimit))
+		return nil, fmt.Errorf("WrongPkgLengthErr: %s", fmt.Errorf("receive header pkg length %d exceed expected or limit size: %d", h.pkgLen, r.respLimit))
 	}
 	//receive body
 	resp := make([]byte, h.pkgLen)
 	if _, err := io.ReadFull(r.c, resp); err != nil {
-		return nil, NewError("ReadResponseBodyErr", err)
+		return nil, fmt.Errorf("ReadResponseBodyErr: [%w]", err)
 	}
 	return resp, nil
 }
